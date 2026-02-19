091f2484e5cf1e21cefbb9437224748f5cf619e1
abdinasir1993 <abdinasir_jirde@hotmail.com>
2026-02-19 11:08:19 +0000

    configured ssl for https redirect

--- terraform/modules/alb/main.tf
+++ terraform/modules/alb/main.tf
@@ -22,8 +22,12 @@ resource "aws_lb_listener" "front_end_listener" {
   port              = "80"
   protocol          = "HTTP"
   default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.frontend_target_group.arn
+    type = "redirect"
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
   }
 }
 
